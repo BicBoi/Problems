84b90aaaecd20f2761cea85cdd2b552f7c3c0be3
BicBoi <austinmatase@gmail.com>
2013-10-21 08:42:16 -0400

    Nags

---
+++ Chapter 3 probs/Chapter 3 probs/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            Console.WriteLine(" Type P for pine, O for oak, M for mahogany.");
+            string input = Console.ReadLine();
+            if (input == "P")
+            {
+                Console.WriteLine("Pine tables cost 100$.");
+            }
+            
+            else if (input == "M")
+            {
+                Console.WriteLine("Mahogany tables cost 310$");
+            }
+            else
+            {
+                Console.WriteLine("Oak tables cost 225$");
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your zip code and see if we can deliver to you.");
+
+            int[] ZipCode = new int[10];
+            ZipCode[0] = 18040;
+            ZipCode[1] = 18042;
+            ZipCode[2] = 18045;
+            ZipCode[3] = 65345;
+            ZipCode[4] = 62435;
+            ZipCode[5] = 61322;
+            ZipCode[6] = 87654;
+            ZipCode[7] = 13554;
+            ZipCode[8] = 12345;
+            ZipCode[9] = 98765;
+
+            string input = Console.ReadLine();
+            for (int i = 0; i < 100; i++)
+            {
+                if (input == ZipCode[i].ToString())
+                {
+                    Console.WriteLine("We can deliver to your zip code.");
+                    
+                }
+                else
+                {
+                    Console.WriteLine("We can't deliver to your zip code.");
+                }
+                break;
+            }
+
+        }
+    }
+}
