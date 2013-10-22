f69f8c7ac0c2cf8a15c97ed03ac8de67ee7df17e
BicBoi <austinmatase@gmail.com>
2013-10-22 08:25:01 -0400

    nags

---
+++ Chapter 3 probs/Carprice/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Carprice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string carPrice = Console.ReadLine();
+            Convert.ToInt32(carPrice);
+
+            if (carPrice < 15000)
+            {
+
+            }
+        }
+    }
+}
---
+++ Chapter 3 probs/chatawhile/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace chatawhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double price;
+            int[] AreaCode = new int[6]{262, 414, 608, 715, 815, 920};
+            double[] rate = new double[6]{0.07, 0.10, 0.05, 0.16, 0.24, 0.14};
+                string code;
+            string minutes;
+
+            Console.WriteLine("Enter Your Area Code.");
+                code = Console.ReadLine();
+            Console.WriteLine("How long was your call in minutes?");
+               minutes = Console.ReadLine();
+
+                for (int i = 0; i < AreaCode.Length; i++)
+                {
+                    
+
+                    if (code == AreaCode[i].ToString)
+                    {
+                        price = rate[i] * Convert.ToInt32(minutes);
+                    }
+                    Console.WriteLine(price);
+                }
+        }
+    }
+}
